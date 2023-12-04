869cfdce5d4f900d119d014924b78fc13ecf27ac
Etienne <etmic6@ulaval.ca>
2023-12-04 00:45:31 -0500

    tests workning

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,52 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "MAX30105.h"
 
-// put function declarations here:
-int myFunction(int, int);
+MAX30105 particleSensor; // initialize MAX30102 with I2C
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  while(!Serial)delay(50); //We must wait for Teensy to come online
+  delay(500);
+  Serial.println("");
+  Serial.println("MAX30102");
+  delay(500);
+
+  // Initialize sensor
+  if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (particleSensor.begin(Wire, I2C_SPEED_FAST) == false);
+  }
+  Serial.println("MAX30105 found.");
+
+  byte ledBrightness = 70; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 69; //Options: 69, 118, 215, 411
+  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int babo = 2;
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  particleSensor.check(); //Check the sensor
+  while (particleSensor.available()) {
+      
+      // read microseconds
+      Serial.print("micros ");
+      Serial.println(micros());
+      // read stored IR
+      Serial.print(">IR:");
+      Serial.println(particleSensor.getFIFOIR());
+      // read stored red
+      Serial.print(">Red:");
+      Serial.println(particleSensor.getFIFORed());
+      // read next set of samples
+      particleSensor.nextSample();      
+  }
+  // Serial.println("allo");
+  
 }
