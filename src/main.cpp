e774af9b30b1a41f444cb8e5f28f04a7ff4bec90
Michiboi29 <emichaud29@gmail.com>
2023-12-07 18:07:01 -0500

    BLE to test

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "MAX30105.h"
+#include <ArduinoBLE.h>
+
+BLEService particleService("19B10010-E8F2-537E-4F6C-D104768A1214"); // Create a BLE service
+
+BLEUnsignedCharCharacteristic irCharacteristic("19B10011-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // IR Characteristic
+BLEUnsignedCharCharacteristic redCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // Red Characteristic
 
 MAX30105 particleSensor; // initialize MAX30102 with I2C
 
 void setup()
 {
   Serial.begin(115200);
-  while(!Serial)delay(50); //We must wait for Teensy to come online
+  while(!Serial) delay(50); //We must wait for Teensy to come online
   delay(500);
   Serial.println("");
   Serial.println("MAX30102");
@@ -29,24 +35,54 @@ void setup()
   int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
+  // BLE setup
+  BLE.begin();
+  BLE.setLocalName("ParticleSensor");
+  BLE.setAdvertisedService(particleService);
+  particleService.addCharacteristic(irCharacteristic);
+  particleService.addCharacteristic(redCharacteristic);
+  BLE.addService(particleService);
+  BLE.advertise();
+
+  Serial.println("BLE Peripheral - ParticleSensor is now advertising.");
 }
 
 void loop() {
-  particleSensor.check(); //Check the sensor
-  while (particleSensor.available()) {
-      
-      // read microseconds
-      Serial.print("micros ");
-      Serial.println(micros());
-      // read stored IR
-      Serial.print(">IR:");
-      Serial.println(particleSensor.getFIFOIR());
-      // read stored red
-      Serial.print(">Red:");
-      Serial.println(particleSensor.getFIFORed());
-      // read next set of samples
-      particleSensor.nextSample();      
+  BLEDevice central = BLE.central();
+  if (central)
+  {
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+
+    while (central.connected())
+    {
+      particleSensor.check(); //Check the sensor
+      while (particleSensor.available()) {
+        // Read IR and Red data
+        uint8_t irValue = particleSensor.getFIFOIR();
+        uint8_t redValue = particleSensor.getFIFORed();
+
+        // read microseconds
+        Serial.print("micros ");
+        Serial.println(micros());
+        // read stored IR
+        Serial.print(">IR:");
+        Serial.print(irValue);
+        // read stored red
+        Serial.print(">Red:");
+        Serial.println(redValue);
+
+        // Update BLE characteristics
+        irCharacteristic.writeValue(irValue);
+        redCharacteristic.writeValue(redValue);
+
+        delay(10); // Add a small delay to avoid overloading BLE
+
+        // read next set of samples
+        particleSensor.nextSample();      
+      } 
+    }
+    Serial.println("Disconnected from central");
   }
-  // Serial.println("allo");
-  
 }
