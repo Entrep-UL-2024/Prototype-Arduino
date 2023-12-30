520ee2b3fa3b098e312fa76e76647bb2fea87bdd
Etienne <etmic6@ulaval.ca>
2023-12-30 01:12:07 -0500

    PRINTS GOOD DATA

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <Wire.h>
 #include "MAX30105.h"
 #include <ArduinoBLE.h>
+#include "spo2_algorithm.h"
 
 #define PRINTS
+// #define USE_BLE
 
 #ifdef PRINTS
 #define Print(msg) Serial.print(msg)
@@ -13,103 +15,10 @@
 #define Println(msg)
 #endif
 
-//  Init BLE
+//  BLE constants
 const char* ble_name = "ParticleSensor";
-unsigned long lastReadTime = 0;
 const unsigned long updateInterval = 500; // Update BLE every 1000ms
-
-BLEService particleService("19B10010-E8F2-537E-4F6C-D104768A1214"); // Create a BLE service
-BLEUnsignedLongCharacteristic irCharacteristic("19B10011-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // IR Characteristic
-BLEUnsignedLongCharacteristic redCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // Red Characteristic
-BLEUnsignedLongCharacteristic currentCharacteristic("19B10013-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // Define BLE Characteristic for Current
-
-
-float calculateSpO2(uint32_t redValue, uint32_t irValue, float a = -17, float b = 104) {
-  // Calculate the ratio of red and IR values
-  float ratio = (float)redValue / (float)irValue;
-
-  // Calculate SpO2 using the ratio
-  float spo2 = a * ratio + b;
-
-  return spo2;
-}
-
-const int RATE_SIZE = 4; // Increase this for more accuracy
-int rates[RATE_SIZE];
-int rateSpot = 0;
-long lastBeat = 0;
-bool checkForBeat(uint32_t irValue) {
-  // Implement your logic to detect a heartbeat using the IR value
-  // Return true if a heartbeat is detected, false otherwise
-  // You can use any algorithm or condition that suits your requirements
-
-  // Example logic: Check if the IR value exceeds a certain threshold
-  const int IR_THRESHOLD = 1000;
-  if (irValue > IR_THRESHOLD) {
-    return true;
-  } else {
-    return false;
-  }
-}
-float calculateHeartRateBPM(uint32_t irValue) {
-    float heartRate = 0.0;
-
-    if (checkForBeat(irValue)) { // Assuming checkForBeat is a function that detects a heartbeat
-        long delta = millis() - lastBeat;
-        lastBeat = millis();
-
-        heartRate = 60 / (delta / 1000.0); // Calculate BPM
-
-        rates[rateSpot++] = (int)heartRate; // Store this reading in the array
-        rateSpot %= RATE_SIZE;
-
-        // Average the last few readings
-        float beatAvg = 0;
-        for (int i = 0; i < RATE_SIZE; i++) {
-            beatAvg += rates[i];
-        }
-        beatAvg /= RATE_SIZE;
-
-        heartRate = beatAvg;
-    }
-
-    return heartRate;
-}
-
-uint32_t calculateHeartRateUINT(uint32_t irValue){
-  // TODO: implement this function
-  float heartRate = calculateHeartRateBPM(irValue);
-  return (uint32_t)heartRate;
-}
-
-void updateBLE(uint32_t irValue, uint32_t redValue, uint32_t currentValue){
-  // Update BLE
-  if (millis() - lastReadTime > updateInterval) {
-    Print(">IRValueBLE:");
-    Println(irValue);
-    Print(">RedValueBLE:");
-    Println(redValue);
-    Print(">CurrentBLE:");
-    Println(currentValue);
-
-    // calculate heart rate
-    float heartRate = calculateHeartRateBPM(irValue);
-    Print(">HeartRateBLE:");
-    Println(heartRate);
-    // calculate SpO2
-    float spo2 = calculateSpO2(redValue, irValue);
-    Print(">SpO2BLE:");
-    Println(spo2);
-
-    irCharacteristic.writeValue(irValue);
-    redCharacteristic.writeValue(redValue);
-    currentCharacteristic.writeValue(currentValue);
-    // update time
-    lastReadTime = millis();
-  }
-}
-
-// Init MAX30102 Oxymeter Sensor
+// Init MAX30102 Oxymeter Sensor constants
 MAX30105 particleSensor; // initialize MAX30102 with I2C
 byte ledBrightness = 60; //Options: 0=Off to 255=50mA
 byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
@@ -117,63 +26,168 @@ byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
 byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
 int pulseWidth = 411; //Options: 69, 118, 215, 411
 int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
-
 // Oxymeter constants
-const int BUFFER_SIZE_OX = 10;
-uint64_t irSum = 0;
-uint32_t irBuffer[BUFFER_SIZE_OX];
-int irBufferIndexOx = 0;
-int irNumSamplesOx = 0;
-uint64_t redSum = 0;
-uint32_t redBuffer[BUFFER_SIZE_OX];
-int redBufferIndexOx = 0;
-int redNumSamplesOx = 0;
-
-void read_data(uint32_t &irValue, uint32_t &redValue){
-  // Read IR and Red data
-  uint32_t _irValue = particleSensor.getIR();
-  uint32_t _redValue = particleSensor.getRed();
-  uint32_t _greenValue = particleSensor.getFIFOGreen();
-  
-  // Update IR circular buffer
-  irSum -= irBuffer[irBufferIndexOx];
-  irSum += _irValue;
-  irBuffer[irBufferIndexOx] = _irValue;
-  irBufferIndexOx = (irBufferIndexOx + 1) % BUFFER_SIZE_OX;
-  if (irNumSamplesOx < BUFFER_SIZE_OX) irNumSamplesOx++;
-
-  // Update Red circular buffer
-  redSum -= redBuffer[redBufferIndexOx];
-  redSum += _redValue;
-  redBuffer[redBufferIndexOx] = _redValue;
-  redBufferIndexOx = (redBufferIndexOx + 1) % BUFFER_SIZE_OX;
-  if (redNumSamplesOx < BUFFER_SIZE_OX) redNumSamplesOx++;
-  
-  // Calculate mean values
-  uint32_t meanIR = abs(irSum / irNumSamplesOx);
-  uint32_t meanRed = abs(redSum / redNumSamplesOx);
-
-  // Update output values
-  irValue = meanIR;
-  redValue = meanRed;
-  
-  // read next set of samples
-  particleSensor.nextSample();
-};
-
-
-// Init Current sensor
+const int BUFFER_SIZE_OX = 100;
+const int BATCH_SIZE = 5;
+const int BUFFER_SIZE_HR = 10;
+const int BUFFER_SIZE_SPO2 = 10;
+// Init Current sensor constants
 // Assuming you're using a 5A ACS712. Adjust the sensitivity accordingly for other models.
 const float ACS712_SENSITIVITY = 0.185; // Sensitivity in V/A (for 5A module)
 const int ACS712_ZERO = 512; // Change this based on your calibration
-
-// Current constants
 const int BUFFER_SIZE_CUR = 100;
+
+// Init BLE
+BLEService particleService("19B10010-E8F2-537E-4F6C-D104768A1214"); // Create a BLE service
+BLEUnsignedLongCharacteristic hRCharacteristic("19B10011-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // IR Characteristic
+BLEUnsignedLongCharacteristic spo2Characteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // Red Characteristic
+BLEUnsignedLongCharacteristic currentCharacteristic("19B10013-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // Define BLE Characteristic for Current
+
+unsigned long lastReadTime = 0;
+void updateBLE(uint32_t hrValue, uint32_t spo2Value, uint32_t currentValue){
+  // Update BLE
+  if (millis() - lastReadTime > updateInterval) {
+    Print(">HRValueBLE:");
+    Println(hrValue);
+    Print(">Spo2ValueBLE:");
+    Println(spo2Value);
+    Print(">CurrentBLE:");
+    Println(currentValue);
+
+    hRCharacteristic.writeValue(hrValue);
+    spo2Characteristic.writeValue(spo2Value);
+    currentCharacteristic.writeValue(currentValue);
+    // update time
+    lastReadTime = millis();
+  }
+}
+
+int32_t hrBuffer[BUFFER_SIZE_HR];
+int32_t spo2Buffer[BUFFER_SIZE_SPO2];
+int64_t hrSum = 0;
+int hrBufferIndex = 0;
+int hrNumSamples = 0;
+int64_t spo2Sum = 0;
+int spo2BufferIndex = 0;
+int spo2NumSamples = 0;
+
+int32_t hrMean = 0;
+int32_t spo2Mean = 0;
+void update_mean_HR_Spo2(int32_t _hr, int8_t _hrValid, int32_t _spo2, int8_t _spo2Valid){
+  if (_hrValid != 0){
+    hrSum -= hrBuffer[hrBufferIndex];
+    hrSum += _hr;
+    hrBuffer[hrBufferIndex] = _hr;
+    hrBufferIndex = (hrBufferIndex + 1) % BUFFER_SIZE_HR;
+    if (hrNumSamples < BUFFER_SIZE_HR) hrNumSamples++;
+    // Calculate mean values
+    int32_t _meanHR = abs(hrSum / hrNumSamples);
+    // Update output values
+    hrMean = _meanHR;
+  }
+  if (_spo2Valid != 0){
+    spo2Sum -= spo2Buffer[spo2BufferIndex];
+    spo2Sum += _spo2;
+    spo2Buffer[spo2BufferIndex] = _spo2;
+    spo2BufferIndex = (spo2BufferIndex + 1) % BUFFER_SIZE_SPO2;
+    if (spo2NumSamples < BUFFER_SIZE_SPO2) spo2NumSamples++;
+    // Calculate mean values
+    int32_t _meanSpo2 = abs(spo2Sum / spo2NumSamples);
+    // Update output values
+    spo2Mean = _meanSpo2;
+  }
+};
+
+uint32_t irBuffer[BUFFER_SIZE_OX];
+uint32_t redBuffer[BUFFER_SIZE_OX];
+
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+void calibrate(){
+  int bufferLength = BUFFER_SIZE_OX; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    Print("red=");
+    Print(redBuffer[i]);
+    Print(", ir=");
+    Println(irBuffer[i]);
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+  update_mean_HR_Spo2(heartRate, validHeartRate, spo2, validSPO2);
+}
+
+void get_data(int batch_size = BATCH_SIZE){
+  
+    int bufferLength = BUFFER_SIZE_OX; //buffer length of 100 stores 4 seconds of samples running at 25sps
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = batch_size; i < bufferLength; i++)
+    {
+      redBuffer[i - batch_size] = redBuffer[i];
+      irBuffer[i - batch_size] = irBuffer[i];
+    }
+
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = (bufferLength - batch_size); i < bufferLength; i++)
+    {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      //send samples and calculation result to terminal program through UART
+      // Print(">red:");
+      // Println(redBuffer[i]);
+      // Print(">ir:");
+      // Println(irBuffer[i]);
+
+      Print(">HR:");
+      Println(heartRate);
+
+      Print(">HRvalid:");
+      Println(validHeartRate);
+
+      Print(">SPO2:");
+      Println(spo2);
+
+      Print(">SPO2Valid:");
+      Println(validSPO2);
+
+      // update mean
+      update_mean_HR_Spo2(heartRate, validHeartRate, spo2, validSPO2);
+
+      Print(">HRMean:");
+      Println(hrMean);
+
+      Print(">SPO2Mean:");
+      Println(spo2Mean);
+
+    }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+}
+
 float sumCurrent = 0.0;
 float currentBuffer[BUFFER_SIZE_CUR];
 int bufferIndexCur = 0;
 int numSamplesCur = 0;
-
 uint32_t readCurrent(){
   int analogValue = analogRead(A0);
   // Convert to voltage (assuming 5V reference and 10-bit ADC)
@@ -194,7 +208,7 @@ uint32_t readCurrent(){
   // Convert current to an integer for BLE transmission
   // This will send the current in milliamps
   uint32_t currentForBLE = (uint32_t)abs(meanCurrent * 1000);
-  
+
   return currentForBLE;
 }
 
@@ -202,12 +216,11 @@ void setup()
 {
   #ifdef PRINTS
   Serial.begin(115200);
-  while(!Serial) delay(10); 
-  // delay(2500); //wait for serial
+  while(!Serial) delay(10);
+  delay(2500); //wait for serial
+  #endif
   Println("");
   Println("MAX30102 Oxymeter Sensor");
-  #endif
-  delay(500);
 
   // Initialize sensor
   if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false) //Use default I2C port, 400kHz speed
@@ -219,12 +232,13 @@ void setup()
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
 
+  #ifdef USE_BLE
   // BLE setup
   BLE.begin();
   BLE.setLocalName(ble_name);
   BLE.setAdvertisedService(particleService);
-  particleService.addCharacteristic(irCharacteristic);
-  particleService.addCharacteristic(redCharacteristic);
+  particleService.addCharacteristic(hRCharacteristic);
+  particleService.addCharacteristic(spo2Characteristic);
   particleService.addCharacteristic(currentCharacteristic);
   BLE.addService(particleService);
   BLE.advertise();
@@ -235,52 +249,40 @@ void setup()
   Print(" | BLE Peripheral - ");
   Print(ble_name);
   Println(" is now advertising.");
+  #endif
 }
 
 void loop() {
+  #ifdef USE_BLE
   BLEDevice central = BLE.central();
-  if (central)
+  bool isCentralConnected = central && central.connected();
+  #else
+  bool isCentralConnected = true;
+  #endif
+  if (isCentralConnected)
   {
+    #ifdef USE_BLE
     Print("Connected to central: ");
     Println(central.address());
-
-    while (central.connected())
+    #endif
+    while (isCentralConnected)
     {
-      particleSensor.check(); //Check the sensor
-      while (particleSensor.available()) {
-        
-        // read data
-        uint32_t irValue, redValue;
-        read_data(irValue, redValue);
-        Print(">IRValue:");
-        Println(irValue);
-        Print(">RedValue:");
-        Println(redValue);
+      calibrate();
+      Println("Calibration done");
+      while (isCentralConnected){
 
-        // calculate heart rate
-        float heartRate = calculateHeartRateBPM(irValue);
-        Print(">HeartRate:");
-        Println(heartRate);
-        // calculate SpO2
-        float spo2 = calculateSpO2(redValue, irValue);
-        Print(">SpO2:");
-        Println(spo2);
+        get_data();
 
-        // calculate current
         uint32_t currentValue = readCurrent();
-        Print(">Current:");
-        Println(currentValue);
-
-        // tests prints with a sinus function
-        uint32_t sinusValue = (uint32_t) (128 + 127 * sin(micros() / 1000000.0 * 2 * PI * 0.5));
-        Print(">Sinus:");
-        Println(sinusValue);
 
         // Update BLE
-        updateBLE(irValue, redValue, currentValue);
-             
+        #ifdef USE_BLE
+        updateBLE(hrMean, spo2Mean, currentValue);
+        #endif
       } 
     }
+    #ifdef USE_BLE
     Println("Disconnected from central");
+    #endif
   }
 }
